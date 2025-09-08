ed060e910d97c5836d544c6859f0ce6e8421cbae
Silent AsasinXD <Aliganbour99@gmail.com>
2025-09-08 15:24:22 +0200

    Blocks now movegit add script.js

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ const Shapes = [
     ]
 ];
 const Colors = [
+    'white',
     'cyan',
     'blue',
     'orange',
@@ -52,9 +53,17 @@ let canvas = document.querySelector('#tetris');
 let context = canvas.getContext("2d");
 context.scale(30, 30);
 
-let pieceObj = generateRandomShape();
+let pieceObj = null;
+let grid = generateGrid();
 console.log(pieceObj);
-showPiece();
+
+function gameState(){
+    if(pieceObj == null){
+        pieceObj = generateRandomShape();
+        showPiece();
+    }
+    moveDown();
+}
 
 function generateRandomShape(){
     let ran = Math.floor(Math.random() * 7);
@@ -72,8 +81,35 @@ function showPiece(){
         for(let j = 0 ; j < newPiece.piece.length ; j++){
             if(newPiece.piece[i][j] == 1){
                 context.fillStyle = newPiece.color;
-                context.fillRect(j , i, 1, 1);
+                context.fillRect(newPiece.cx + j, newPiece.cy + i, 1, 1);
             }
         }
     }
 }
+
+function moveDown(){
+    pieceObj.cy += 1 ;
+    drawGrid();
+}
+
+function generateGrid(){
+    let grid = [];
+    for(let i = 0 ; i < rows ; i++){
+        grid.push([]);
+        for(let j = 0 ; j < cols ; j++){
+            grid[i].push(i);
+        }
+    }
+    return grid ;
+}
+
+function drawGrid(){
+    for(let r = 0 ; r < grid.length ; r++){
+        for(let c = 0 ; c < grid[r].length ; c++){
+            context.fillStyle = "#F2F2F2";
+            context.fillRect(c, r, 1, 1);
+        }
+    }
+    showPiece();
+}
+setInterval(gameState,500);
